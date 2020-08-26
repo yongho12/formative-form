988f7ece3f67d04ade527ff9420474e6370f8ec2
Andrew <travers.drew@gmail.com>
2020-08-26 16:30:08 -0400

    Completed home

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.set("view engine", "pug");
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 const users = [
   {
@@ -17,6 +12,18 @@ const users = [
     email: "jill.jack@gmail.com"
   }
 ];
+
+app.set("view engine", "pug");
+app.use(cookieParser());
+const csrfProtection = csrf({ cookie: true })
+
+app.get("/", (req, res) => {
+  res.render('index.pug', {users});
+});
+
+app.get('/create', csrfProtection, (req, res) => {
+  res.render('index', { title: 'Create Form', csrfToken: req.csrfToken() });
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
